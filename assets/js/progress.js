c041ad2cd795013ae11754d4ec232bb797940df7
Debottam Kundu <debottam.kundu330@gmail.com>
2020-12-04 11:03:01 +0100

    added progress bar

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,19 +1,51 @@
-let prog = document.getElementById('progress');
-
-let body = document.body,
-    html = document.documentElement;
-
-let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-const setProgress = () => {
-   let scrollFromTop = (document.documentElement.scrollTop || body.scrollTop) + html.clientHeight;
-   let width = scrollFromTop / height * 100 + '%';
-
-   console.log('scroll', html.clientHeight, body.scrollTop);
-
-   prog.style.width = width;
-}
-
-window.addEventListener('scroll', setProgress);
-
-setProgress();
+window.youAreHere = (function() {
+  // Element creation
+  var createProgressBar = function() {
+    var newProgressBar = document.createElement("div");
+    newProgressBar.id = 'progressBar';
+    document.body.appendChild(newProgressBar);
+    
+    newProgressBar.style.height = '3px';
+    newProgressBar.style.width = 0;
+    newProgressBar.style.background = '#ff0000';
+    newProgressBar.style.color = '#ff0000';
+    newProgressBar.style.top = 0;
+    newProgressBar.style.right = 'auto';
+    newProgressBar.style.bottom = 'auto';
+    newProgressBar.style.left = 0;
+    newProgressBar.style.position = 'fixed';
+    newProgressBar.style.boxShadow = '0 0 3px';
+    newProgressBar.style.zIndex = 9999;
+    newProgressBar.style.pointerEvents = 'none';
+    
+  };
+  
+  document.addEventListener('DOMContentLoaded', createProgressBar());
+  
+  // Progress calculations and setting
+  var calculateProgress = function() {
+    var scrolledPixels = document.documentElement.scrollTop || document.body.scrollTop;
+    var fullHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    var viewportHeight = window.innerHeight;
+    
+    currentProgress = scrolledPixels / (fullHeight - viewportHeight ) * 100;
+    
+    document.getElementById('progressBar').style.width = currentProgress + '%';
+  }
+  
+  document.addEventListener('scroll', calculateProgress);
+  
+  // Settings function
+  return (function(settings) {
+    var progressBar = document.getElementById('progressBar');
+    
+    progressBar.style.height = settings.height || '3px';
+    progressBar.style.background = settings.color || '#ff0000';
+    progressBar.style.color = settings.color || '#ff0000';
+    if (settings.easing === true) {
+      progressBar.style.transition = 'width .3s cubic-bezier(0.77, 0, 0.175, 1)';
+    } else {
+      progressBar.style.transition = 'none';
+    }
+  });
+})();
