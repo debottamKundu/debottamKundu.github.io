cc57a92dc5b04c3989bf27e8df4d57799f049dc1
Debottam Kundu <debottam.kundu330@gmail.com>
2020-12-14 10:59:32 +0100

    added google analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,11 @@
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FZJBBNP35S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FZJBBNP35S');
+</script>
